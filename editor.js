f8b3f90c2809e0961ee0cf292178bb53870fe1a2
Pottow <33297095+Pottow@users.noreply.github.com>
2022-11-20 15:14:46 +0000

    Add files via upload

--- editor.js
+++ editor.js
@@ -3,3 +3,7 @@ function FirstFunction(){
     console.log("hello world")
     document.getElementById("executeFirstFunction").style.background = "#4CAF50";
 }
+
+function embedWebglPlease (embedWebgl){
+embedWebgl.style.height=embedWebgl.contentWindow.document.body.scrollHeight+"px";
+}
