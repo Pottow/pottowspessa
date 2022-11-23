df3876bf2cc3e4a25df8d85c8f27445036da5a8a
Pottow <33297095+Pottow@users.noreply.github.com>
2022-11-23 13:19:19 +0000

    Graphic resizing and repo linking

--- editor.js
+++ editor.js
@@ -7,3 +7,29 @@ function FirstFunction(){
 function embedWebglPlease (embedWebgl){
 embedWebgl.style.height=embedWebgl.contentWindow.document.body.scrollHeight+"px";
 }
+
+
+
+function scaleImages(){
+     thesisWebGL = document.getElementById("thesisWebGL");
+     thesisWebGL.height = window.innerHeight /1.4625;
+     thesisWebGL.width = window.innerWidth /1.92;
+
+BotressControlPanel = document.getElementById("BotressControlPanel");
+BotressControlPanel.height = window.innerHeight /2.34;
+BotressControlPanel.width = window.innerWidth /2.34;
+
+
+BotressInVoice = document.getElementById("BotressInVoice");
+BotressInVoice.height = window.innerHeight /9;
+BotressInVoice.width = window.innerWidth /7.85;
+
+BotressOnline = document.getElementById("BotressOnline");
+BotressOnline.height = window.innerHeight /13.6;
+BotressOnline.width = window.innerWidth /8.06;
+
+
+}
+
+
+window.onload = scaleImages();
