f01e2b1956d9731401e96a359136ea3560bc6b4f
Pottow <33297095+Pottow@users.noreply.github.com>
2023-05-19 15:28:49 +0100

    Fixing everything with style

--- editor.js
+++ editor.js
@@ -15,87 +15,6 @@ function ifMobile(){
 
 
     if (oneTimeMediaQuery.matches){
-        // $(".grid-container").css("grid-template-columns","50% 50%");
-        // $(".grid-container").css("grid-template-rows","auto");
-        // $(".grid-container").css("padding","10px");
-
-        // $(".pageTitle").css("grid-column-start","1");
-        // $(".pageTitle").css("grid-column-end","3");
-        // $(".pageTitle").css("text-align","center");
-
-        // $(".navbarColour").css("grid-column-start","1");
-        // $(".navbarColour").css("grid-column-end","3");
-
-        // $(".logo").css("grid-column","1");
-        // $(".logo").css("grid-row","1");
-        // $(".logo").css("position","relative");
-        // $(".logo").css("margin","5px");
-        // $(".section_1_text").css("justify-content","left");
-
-        // $(".section_1_text").css("margin-left","0cm");
-        // $(".section_1_text").css("grid-column-start","1");
-        // $(".section_1_text").css("grid-column-end","3");
-        // $(".section_1_text").css("grid-row-start","2");
-        // $(".section_1_text").css("grid-row-end","3");
-        // $(".section_1_text").css("width","1fr");
-        // $(".section_1_text").css("text-align","center");
-        
-        // $(".section_1_graphic").css("margin-left","0cm");
-        // $(".section_1_graphic").css("grid-column-start","1");
-        // $(".section_1_graphic").css("grid-column-end","3");
-        // $(".section_1_graphic").css("grid-row-start","3");
-        // $(".section_1_graphic").css("grid-row-end","4");
-        // $(".section_1_graphic").css("flex-direction","column");     
-        // $(".section_1_graphic").css("flex-grow","1fr");
-        // $(".section_1_graphic").css("flex-wrap","wrap");
-        // $(".section_1_graphic").css("flex-basis","10%");
-        // $(".section_1_graphic").css("align-items","center");
-        // $(".section_1_graphic").css("display","flex");
-
-
-        // $(".section_2_text").css("margin-left","0cm");
-        // $(".section_2_text").css("grid-column-start","1");
-        // $(".section_2_text").css("grid-column-end","3");
-        // $(".section_2_text").css("grid-row-start","4");
-        // $(".section_2_text").css("grid-row-end","5");
-        // $(".section_2_text").css("width","1fr");
-        // $(".section_2_text").css("text-align","center");
-
-        // $(".section_2_graphic").css("margin-left","0cm");
-        // $(".section_2_graphic").css("margin-top","-30px");
-        // $(".section_2_graphic").css("grid-column-start","1");
-        // $(".section_2_graphic").css("grid-column-end","3");
-        // $(".section_2_graphic").css("grid-row-start","5");
-        // $(".section_2_graphic").css("grid-row-end","6");
-        // $(".section_2_graphic").css("text-align","center");
-        // $(".section_2_graphic").css("display","flex");
-        // $(".section_2_graphic").css("margin-left","auto");
-        // $(".section_2_graphic").css("margin-right","auto");
-
-        // $(".section_3_text").css("margin-left","0cm");
-        // $(".section_3_text").css("grid-column-start","1");
-        // $(".section_3_text").css("grid-column-end","3");
-        // $(".section_3_text").css("grid-row-start","6");
-        // $(".section_3_text").css("grid-row-end","7");
-        // $(".section_3_text").css("width","1fr");
-        // $(".section_3_text").css("text-align","center");
-        // $(".section_3_text").css("list-style-position","inside");
-        // $("#ExampleNamesListIntro").css("text-align","left");
-        // $("#ExampleNamesList").css("text-align","left");
-        // $("#ExampleNamesList").css("margin","0cm");
-        // $("#ExampleNamesList").css("padding-left","5px");
-        
-        // $(".section_3_graphic").css("margin-left","0cm");
-        // $(".section_3_graphic").css("grid-column-start","1");
-        // $(".section_3_graphic").css("grid-column-end","3");
-        // $(".section_3_graphic").css("grid-row-start","7");
-        // $(".section_3_graphic").css("grid-row-end","8");
-        // $(".section_3_graphic").css("position","relative");
-        // $(".section_3_graphic").css("margin","0cm");
-        // $(".section_3_graphic").css("text-align","center");
-        // $(".section_3_graphic").css("display","flex");
-        // $(".section_3_graphic").css("margin-left","auto");
-        // $(".section_3_graphic").css("margin-right","auto");
 
         thesisWebGL = document.getElementById("thesisWebGL");
         TTCTheory = document.getElementById("TTCTheory");
@@ -106,9 +25,6 @@ function ifMobile(){
         LogoGraphic = document.getElementById("LogoGraphic");
         
 
-
-        // thesisWebGL.width = 0;
-        // thesisWebGL.height = 0;
         thesisWebGL.remove();
 
         // TTCTheory.height = window.innerHeight /12;
@@ -119,23 +35,19 @@ function ifMobile(){
     
         // BotressInVoice.height = window.innerHeight /9;
         // BotressInVoice.width = window.innerWidth /1.85;
-        // BotressInVoice.height = 0;
-        // BotressInVoice.width = 0;
         BotressInVoice.remove();
     
         // BotressOnline.height = window.innerHeight /15.6;
         // BotressOnline.width = window.innerWidth /2.06;
-        // BotressOnline.height = 0;
-        // BotressOnline.width = 0;
         BotressOnline.remove();
     
         // RandomCimNames.height = window.innerHeight /6.5;
         RandomCimNames.width = window.innerWidth /1.11;
 
         if (window.innerWidth <= 400){
-            LogoGraphic.width = 0;
-            LogoGraphic.height = 0;
+            LogoGraphic.remove();
         }
+        
     }
 
 }
@@ -169,6 +81,7 @@ function scaleImages(){
    
         RandomCimNames.height = window.innerHeight /3.5;
         RandomCimNames.width = window.innerWidth /2.5;
+        
     }
 
 }
