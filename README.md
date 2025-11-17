5630be28e0c729b3461b0efe55d2127ab4b3ae3b
Pottow <gamesbaker777@gmail.com>
2025-11-17 14:30:26 +0000

    Minor refresh and review
    This project hadn't been updated in a while. I have done some basic cleanup in preparation for an overhaul and version 2

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# pottowspessa
-Testing website hosting on gitPages
+# Pottow's Pessa
+A test website on gitPages:
 https://pottow.github.io/pottowspessa/
+
+
+The initial idea behind this project was to create a minimal site from base principles. The codebase uses basic CCS, HTML, and Javascript without an overarching framework like React, Vue, or Angular.
+
+It also acts as a means of keeping track of casual independent projects in a portfolio.
+
+Currently the site is significantly outdated in terms of the portfolio and underlying code. So it will be changing frequently as it is updated.
--- editor.js
+++ editor.js
@@ -1,85 +1,84 @@
-function ifMobile(){
+const mobileMediaQuery = window.matchMedia('(hover: none), (max-width: 900px),(pointer:coarse)');
 
-    const oneTimeMediaQuery = window.matchMedia('(hover: none), (max-width: 900px),(pointer:coarse)');
+function ifMobile() {
+
+
     //not actually one time, will repeat on refresh/resize, but not actively listening itself
 
 
-    if (oneTimeMediaQuery.matches){
+    if (mobileMediaQuery.matches) {
 
         ThesisPic = document.getElementById("ThesisPic");
-        TTCTheory = document.getElementById("TTCTheory");
+        TTCTheory = document.getElementById("TTCTheoryEyeball");
         BotressControlPanel = document.getElementById("BotressControlPanel");
         BotressInVoice = document.getElementById("BotressInVoice");
         BotressOnline = document.getElementById("BotressOnline");
         RandomCimNames = document.getElementById("RandomCimNames");
         LogoGraphic = document.getElementById("LogoGraphic");
-        
 
-        ThesisPic.width = window.innerWidth /1.2;
+
+        ThesisPic.width = window.innerWidth / 1.2;
 
         // TTCTheory.height = window.innerHeight /12;
-        TTCTheory.width = window.innerWidth /1.1;
-    
+        TTCTheory.width = window.innerWidth / 1.1;
+
         // BotressControlPanel.height = window.innerHeight /6;
-        BotressControlPanel.width = window.innerWidth /1.1;
-    
+        BotressControlPanel.width = window.innerWidth / 1.1;
+
         // BotressInVoice.height = window.innerHeight /9;
         // BotressInVoice.width = window.innerWidth /1.85;
         BotressInVoice.remove();
-    
+
         // BotressOnline.height = window.innerHeight /15.6;
         // BotressOnline.width = window.innerWidth /2.06;
         BotressOnline.remove();
-    
-        // RandomCimNames.height = window.innerHeight /6.5;
-        RandomCimNames.width = window.innerWidth /1.11;
 
-        if (window.innerWidth <= 400){
+        // RandomCimNames.height = window.innerHeight /6.5;
+        RandomCimNames.width = window.innerWidth / 1.11;
+
+        if (window.innerWidth <= 400) {
             LogoGraphic.remove();
         }
-        
+
     }
 
 }
 
-function scaleImages(){
+function scaleImages() {
 
-    const oneTimeMediaQuery = window.matchMedia('(hover: none), (max-width: 900px),(pointer:coarse)');
-    if (!oneTimeMediaQuery.matches){
+    if (!mobileMediaQuery.matches) {
 
-    ThesisPic = document.getElementById("ThesisPic");
-    TTCTheory = document.getElementById("TTCTheory");
-    BotressControlPanel = document.getElementById("BotressControlPanel");
-    BotressInVoice = document.getElementById("BotressInVoice");
-    BotressOnline = document.getElementById("BotressOnline");
-    RandomCimNames = document.getElementById("RandomCimNames");
-        
-        ThesisPic.height = window.innerHeight /1.5;
-        ThesisPic.width = window.innerWidth /1.92;
+        ThesisPic = document.getElementById("ThesisPic");
+        TTCTheory = document.getElementById("TTCTheoryEyeball");
+        BotressControlPanel = document.getElementById("BotressControlPanel");
+        BotressInVoice = document.getElementById("BotressInVoice");
+        BotressOnline = document.getElementById("BotressOnline");
+        RandomCimNames = document.getElementById("RandomCimNames");
+
+        ThesisPic.height = window.innerHeight / 1.5;
+        ThesisPic.width = window.innerWidth / 1.92;
 
         // TTCTheory.height = window.innerHeight /6.4;
-        TTCTheory.width = window.innerWidth /2.5;
-   
-        BotressControlPanel.height = window.innerHeight /2.34;
-        BotressControlPanel.width = window.innerWidth /2.34;
-   
-        BotressInVoice.height = window.innerHeight /9;
-        BotressInVoice.width = window.innerWidth /7.85;
-   
-        BotressOnline.height = window.innerHeight /13.6;
-        BotressOnline.width = window.innerWidth /8.06;
-   
-        RandomCimNames.height = window.innerHeight /3.5;
-        RandomCimNames.width = window.innerWidth /2.5;
-        
-    }
+        TTCTheory.width = window.innerWidth / 2.5;
 
+        BotressControlPanel.height = window.innerHeight / 2.34;
+        BotressControlPanel.width = window.innerWidth / 2.34;
+
+        BotressInVoice.height = window.innerHeight / 9;
+        BotressInVoice.width = window.innerWidth / 7.85;
+
+        BotressOnline.height = window.innerHeight / 13.6;
+        BotressOnline.width = window.innerWidth / 8.06;
+
+        RandomCimNames.height = window.innerHeight / 3.5;
+        RandomCimNames.width = window.innerWidth / 2.5;
+    }
 }
 
 
-window.onload = function(){
-                scaleImages();
-                ifMobile();
-                }
+window.onload = function () {
+    scaleImages();
+    ifMobile();
+}
 window.onresize = scaleImages;
 window.on
